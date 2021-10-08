32a4cfbf82aa5cd30d2e91712882dd2593843524
rarael <rarael@live.unc.edu>
2021-10-08 11:27:27 -0400

    mild changes

--- src/mild/mild_1.js
+++ src/mild/mild_1.js
@@ -9,7 +9,11 @@
  * see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
  */
 export function sumToString(a, b) {
-
+    var result = a + b
+    var aToString = a.toString;
+    var bToString = b.toString;
+    var answer = aToString + " + " + bToString + " = " + result;
+    return answer;
 }
 
 
@@ -24,7 +28,11 @@ export function sumToString(a, b) {
  *
  */
 export function getIncreasingArray(startNumber, endNumber) {
-
+    let array = [];
+    for(var i = 3; i <= endNumber; i++) {
+        array.push(i);
+    }
+    return array;
 }
 
 /**
@@ -35,7 +43,16 @@ export function getIncreasingArray(startNumber, endNumber) {
  * and https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
  */
 export function maxAndMin(numbers) {
-
+    var min = numbers[0];
+    var max = numbers[0];
+    for(var i = 1; i < numbers.length; i++) {
+        if (numbers[i] < min) {
+            min = numbers[i];
+        } else if (numbers[i] > max) {
+            max = numbers[i];
+        }
+    }
+    return (min, max);
 }
 
 /**
