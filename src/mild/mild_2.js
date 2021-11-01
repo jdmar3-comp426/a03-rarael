09370db8922eea850e7970c3f39854c3010c8608
rarael <rarael@live.unc.edu>
2021-11-01 16:17:03 -0400

    removeKeyNonDestructive

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -68,7 +68,9 @@ export function removeKey(object, key) {
  If only `removeKeyNonDestructive` was called, nothing would have changed.
  */
 export function removeKeyNonDestructive(object, key) {
-
+   let returnObject = Object.assign({}, object);
+   delete returnObject[key]
+   return returnObject
 }
 
 /**
