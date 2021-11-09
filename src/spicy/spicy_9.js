0a2c549d9c9c5058055e86003ef968e8ae419390
rarael <rarael@live.unc.edu>
2021-11-09 08:42:18 -0500

    finishSpicy

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -173,7 +173,7 @@ export const filter = (arr, test) => {
  *   odd numbers. Use the "everyEven" function in this function.
  */
 export const allEvensAreOdd = (arr) => {
-
+    return everyEven(arr, x => x % 2 != 0)
 };
 
 
@@ -183,7 +183,7 @@ export const allEvensAreOdd = (arr) => {
  *   array is an odd number. Use the "someEven" function in this function.
  */
 export const anEvenIsOdd = (arr) => {
-
+    return someEven(arr, x => x % 2 != 0 )
 };
 
 
@@ -194,5 +194,6 @@ export const anEvenIsOdd = (arr) => {
  *   pass the test. You must use the filter function.
  */
 export const hasExactly = (arr, test, n) => {
-
+    let array = filter(arr, test).pass
+    return array.length == n
 };
